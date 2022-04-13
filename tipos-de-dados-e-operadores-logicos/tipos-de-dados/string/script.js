5d513c5f7cc79ec2b927c61d976cf13f9f790f26
Leonardo Xavier <gxavier.leonardo@gmail.com>
2022-04-13 18:03:36 -0300

    tipos: Mais Sobre Strings

--- tipos-de-dados-e-operadores-logicos/tipos-de-dados/string/script.js
+++ tipos-de-dados-e-operadores-logicos/tipos-de-dados/string/script.js
@@ -5,3 +5,10 @@ console.log(typeof 'Eu queria comprar um carro');
 console.log(typeof `Teste`);
 console.log(typeof "Infinity");
 console.log(typeof Infinity);
+
+/** Mais Sobre Strings */
+console.log("Primeira linha\nSegunda linha");
+console.log("O meu nome é 'Leonardo'");
+console.log('O nome dela é "Roberta"');
+console.log(`A multiplicação de 5 por 3 é ${5 * 3}`);
+console.log("O " + "meu " + "nome " + "é " + "Leonardo.");
