49193d68858301231dc3566baa41cd33d82f4bf3
Leonardo Xavier <gxavier.leonardo@gmail.com>
2022-04-13 18:14:20 -0300

    tipos: Comparações com Booleans

--- tipos-de-dados-e-operadores-logicos/tipos-de-dados/boolean/script.js
+++ tipos-de-dados-e-operadores-logicos/tipos-de-dados/boolean/script.js
@@ -3,3 +3,14 @@ console.log(typeof true);
 console.log(typeof false);
 console.log(typeof (10 > 2));
 console.log(typeof (5 > 200));
+
+/** Comparações com Booleans */
+console.log(1 > 2);
+console.log(5 < 10);
+console.log(3 >= 3);
+console.log(5 <= 4);
+
+console.log(5 == 4);
+console.log("Leonardo" != "Leonardo");
+console.log(3 === "3");
+console.log(4 === 4);
