499a617556ea79b3efededa55176a36f70689847
Leonardo Xavier <gxavier.leonardo@gmail.com>
2022-04-13 17:58:00 -0300

    tipos: Tipos de Dados: String

---
+++ tipos-de-dados-e-operadores-logicos/tipos-de-dados/string/script.js
@@ -0,0 +1,7 @@
+/** String */
+console.log(typeof "Oi, meu nome é Leonardo");
+// prettier-ignore
+console.log(typeof 'Eu queria comprar um carro');
+console.log(typeof `Teste`);
+console.log(typeof "Infinity");
+console.log(typeof Infinity);
