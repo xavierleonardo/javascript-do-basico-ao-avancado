2adbcad8d189779fb28d73d9f272f9aa69b7c37c
Leonardo Xavier <gxavier.leonardo@gmail.com>
2022-04-13 17:48:29 -0300

    tipos: Operações Aritméticas com Number

--- tipos-de-dados-e-operadores-logicos/tipos-de-dados/number/script.js
+++ tipos-de-dados-e-operadores-logicos/tipos-de-dados/number/script.js
@@ -1,3 +1,12 @@
+/** Number */
 console.log(typeof 5);
 console.log(typeof 12.7);
 console.log(typeof -123);
+
+/** Operações Aritméticas com Number */
+console.log(typeof (2 + 3));
+console.log(typeof (5 - 12));
+console.log(typeof (4 * 4));
+console.log(typeof (5 / 3));
+console.log(typeof (20 % 2));
+console.log(typeof (21 % 2));
