cee91699ba483baeea51a6973e1ba9caa81fc352
Leonardo Xavier <gxavier.leonardo@gmail.com>
2022-06-08 23:26:16 -0300

    estruturas: Estrutura de Controle: if

---
+++ estruturas-de-programacao/estrutura-de-controle-if/script.js
@@ -0,0 +1,10 @@
+let idade = 19;
+if (idade == 19) console.log('A idade é = 19');
+if (idade > 25) console.log('A idade é > 25');
+
+let nome = 'Leonardo';
+if (nome == 'Leonardo' && idade > 10) console.log('Liberado!');
+
+let passaporte = true;
+if ((nome == 'Leonardo' && idade > 30) || passaporte)
+  console.log('Liberado 2!');
