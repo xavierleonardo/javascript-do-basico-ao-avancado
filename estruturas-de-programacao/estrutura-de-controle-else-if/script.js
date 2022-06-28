b0ffebc3b8d153b8416eb7d7bd45994a7e00fc7a
Leonardo Xavier <gxavier.leonardo@gmail.com>
2022-06-27 21:22:19 -0300

    estruturas: Estrutura de Controle: else if

---
+++ estruturas-de-programacao/estrutura-de-controle-else-if/script.js
@@ -0,0 +1,15 @@
+let nome = 'Leonardo';
+
+if (nome != undefined) {
+  console.log('Nome está definido');
+} else if (nome == 'Leonardo' && nome.length > 5 && idade == 50) {
+  console.log('O nome é Leonardo');
+} else {
+  console.log('Não é Leonardo!');
+}
+
+if (1 > 2) {
+  console.log('Teste');
+} else if (1 == 1) {
+  console.log('Testando');
+}
