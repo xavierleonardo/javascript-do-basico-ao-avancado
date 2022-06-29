6fab814e3fbea2337ba7a8e7503a7ae9338b0802
Leonardo Xavier <gxavier.leonardo@gmail.com>
2022-06-28 22:49:26 -0300

    estruturas: Estrutura de Controle: switch

---
+++ estruturas-de-programacao/estrutura-de-controle-switch/script.js
@@ -0,0 +1,18 @@
+let nome = 'Leonardo';
+
+switch (nome) {
+  case 'Leonardo':
+    console.log('O nome é Leonardo');
+    break;
+  case 'João':
+    console.log('O nome é João');
+    break;
+  default:
+    console.log('O nome não foi encontrado');
+}
+
+// if (nome == 'Leonardo') {
+//   console.log('O nome é Leonardo');
+// } else {
+//   console.log('O nome não foi encontrado');
+// }
