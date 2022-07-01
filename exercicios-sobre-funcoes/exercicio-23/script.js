588a45e8404c98476ff74a47e246d1cabbab6f92
Leonardo Xavier <gxavier.leonardo@gmail.com>
2022-07-01 17:57:10 -0300

    exercicios: Exercício 23

---
+++ exercicios-sobre-funcoes/exercicio-23/script.js
@@ -0,0 +1,7 @@
+const length = (str) => {
+  if (str.length > 10) console.log('Texto muito longo');
+  else console.log('Texto dentro do limite');
+};
+
+length('bsalfirglncgagasvflccgfs');
+length('');
