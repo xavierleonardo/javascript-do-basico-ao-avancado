a5129e3f924894a0601362f901f43f58239e1536
Leonardo Xavier <gxavier.leonardo@gmail.com>
2022-07-03 15:35:46 -0300

    objetos: Métodos de Arrays: Shift e Unshift

---
+++ arrays-e-objetos/metodos-de-arrays-shift-e-unshift/script.js
@@ -0,0 +1,8 @@
+let carros = ['BMW', 'Audi', 'VW', 'Fiat'];
+let carroRemovido = carros.shift();
+console.log(carroRemovido);
+console.log(carros);
+
+carros.unshift('Gurgel');
+console.log(carros);
+console.log(carros[0]);
