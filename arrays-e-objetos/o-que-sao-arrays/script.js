18a716bf43743e0d5e3a7cd5377b0e0a21a510ef
Leonardo Xavier <gxavier.leonardo@gmail.com>
2022-07-03 14:58:36 -0300

    objetos: O Que São Arrays

---
+++ arrays-e-objetos/o-que-sao-arrays/script.js
@@ -0,0 +1,10 @@
+let array = [1, 4, 5, 6, 7];
+let nomes = ['Leonardo', 'João', 'Aléxia'];
+let bool = [true, false, true];
+
+let misturado = [1, 'Leonardo', true];
+
+console.log(array);
+console.log(nomes);
+console.log(bool);
+console.log(misturado);
