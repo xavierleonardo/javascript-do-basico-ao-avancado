30b942f8a969e1ac93e464889c8e2f3caec42f63
Leonardo Xavier <gxavier.leonardo@gmail.com>
2022-07-03 15:19:20 -0300

    objetos: Copiando Propriedades de um Objeto

---
+++ arrays-e-objetos/copiando-propriedades-de-um-objeto/script.js
@@ -0,0 +1,6 @@
+let carro = { portas: 2, portaMalas: '200l', motor: '2.0' };
+let adicionais = { tetoSolar: true, arCondicionado: true };
+
+console.log(carro);
+Object.assign(carro, adicionais);
+console.log(carro);
