292dd329c799704f049be1b22a0dec6cf38b3e81
Leonardo Xavier <gxavier.leonardo@gmail.com>
2022-07-05 22:06:29 -0300

    oo: Herança

---
+++ conceitos-de-orientacao-a-objetos/heranca/script.js
@@ -0,0 +1,23 @@
+class Mamifero {
+  constructor(patas) {
+    this.patas = patas;
+  }
+}
+
+let coiote = new Mamifero(4);
+console.log(coiote.patas);
+
+class Cachorro extends Mamifero {
+  constructor(patas, raca) {
+    super(patas);
+    this.raca = raca;
+  }
+
+  latir() {
+    console.log('Au au');
+  }
+}
+
+let pug = new Cachorro(4, 'Pug');
+console.log(pug.patas);
+pug.latir();
