f8994c30e416b64a98dd870c42dbb0eb2f68c3df
Leonardo Xavier <gxavier.leonardo@gmail.com>
2022-07-08 14:45:48 -0300

    regex: Operador Plus +

---
+++ expressoes-regulares/operador-plus/script.js
@@ -0,0 +1,6 @@
+const umOuMaisNumeros = /\d+/;
+console.log(umOuMaisNumeros.test('1518'));
+console.log(umOuMaisNumeros.test(''));
+console.log(umOuMaisNumeros.test('asdasdas'));
+console.log(umOuMaisNumeros.test('1'));
+console.log(umOuMaisNumeros.test('1332143565363'));
