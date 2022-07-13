ea851f4c94ce4a66ae0165641f6afe28582bf6e4
Leonardo Xavier <gxavier.leonardo@gmail.com>
2022-07-12 22:32:59 -0300

    navegador: Largura e Altura dos Elementos

---
+++ javascript-e-o-navegador/largura-e-altura-dos-elementos/js/scripts.js
@@ -0,0 +1,5 @@
+let element = document.getElementById('titulo-principal');
+console.log('largura: ' + element.offsetWidth);
+console.log('altura: ' + element.offsetHeight);
+console.log('largura: ' + element.clientWidth);
+console.log('altura: ' + element.clientHeight);
