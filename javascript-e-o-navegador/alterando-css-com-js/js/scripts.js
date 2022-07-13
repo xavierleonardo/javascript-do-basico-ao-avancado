742496b6d76d5a00e321a25f59edf1a71ea0c3de
Leonardo Xavier <gxavier.leonardo@gmail.com>
2022-07-12 22:40:02 -0300

    navegador: Alterando CSS com JS

---
+++ javascript-e-o-navegador/alterando-css-com-js/js/scripts.js
@@ -0,0 +1,7 @@
+let element = document.getElementById('titulo-principal');
+
+setTimeout(() => {
+  element.style.color = 'red';
+  element.style.backgroundColor = 'yellow';
+  element.style.width = '400px';
+}, 1000);
