fa720cede5757cbd1d0c8c25e20c824c3b778370
Leonardo Xavier <gxavier.leonardo@gmail.com>
2022-07-13 18:35:38 -0300

    eventos: Mais Sobre Eventos do Mouse

---
+++ eventos-com-javascript/mais-sobre-eventos-do-mouse/js/scripts.js
@@ -0,0 +1,15 @@
+let btn1 = document.querySelector('#btn1');
+let btn2 = document.querySelector('#btn2');
+
+btn1.addEventListener('mousedown', function (e) {
+  console.log('Apertou o botão');
+});
+
+btn2.addEventListener('dblclick', function (e) {
+  console.log('Clicou duas vezes');
+});
+
+btn2.addEventListener('contextmenu', function (e) {
+  e.preventDefault();
+  console.log('Botão direito');
+});
