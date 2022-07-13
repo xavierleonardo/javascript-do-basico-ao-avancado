b15377e4f1c5aeefc3d3b5be190339f2f0f19230
Leonardo Xavier <gxavier.leonardo@gmail.com>
2022-07-12 22:22:21 -0300

    navegador: Criando Elementos

---
+++ javascript-e-o-navegador/criando-elementos/js/scripts.js
@@ -0,0 +1,11 @@
+let lista = document.createElement('ul');
+for (let i = 0; i < 5; i++) {
+  let item = document.createElement('li');
+  let texto = document.createTextNode('texto lista ' + i);
+
+  item.appendChild(texto);
+  lista.appendChild(item);
+}
+
+let container = document.getElementById('container-principal');
+container.appendChild(lista);
