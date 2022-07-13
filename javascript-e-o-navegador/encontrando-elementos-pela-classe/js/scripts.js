9075f653889a7df58b9481d82bf36be68287c6c2
Leonardo Xavier <gxavier.leonardo@gmail.com>
2022-07-12 21:51:09 -0300

    navegador: Encontrando Elementos Pela Classe

---
+++ javascript-e-o-navegador/encontrando-elementos-pela-classe/js/scripts.js
@@ -0,0 +1,4 @@
+console.log(document.getElementsByTagName('li'));
+console.log(document.getElementsByClassName('itens-azuis'));
+console.log(document.getElementsByClassName('itens-vermelhos'));
+console.log(document.getElementsByClassName('itens'));
