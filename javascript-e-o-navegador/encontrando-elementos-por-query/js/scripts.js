4e60703f97b3133817f3d5edfc5f13b0265e99e1
Leonardo Xavier <gxavier.leonardo@gmail.com>
2022-07-12 21:57:27 -0300

    navegador: Encontrando Elementos Por Query

---
+++ javascript-e-o-navegador/encontrando-elementos-por-query/js/scripts.js
@@ -0,0 +1,5 @@
+console.log(document.querySelector('#container-principal h1'));
+console.log(document.querySelector('#segundo-container h1'));
+console.log(document.querySelector('div div p'));
+console.log(document.querySelector('footer h2'));
+console.log(document.querySelector('ul .itens-azuis'));
