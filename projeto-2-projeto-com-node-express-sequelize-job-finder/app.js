ed5f23cae5ef6dbe293cc5bf4af9eb1b322dbab6
Leonardo Xavier <gxavier.leonardo@gmail.com>
2022-07-22 22:38:28 -0300

    projeto2: Conectando ao Banco de Dados

--- projeto-2-projeto-com-node-express-sequelize-job-finder/app.js
+++ projeto-2-projeto-com-node-express-sequelize-job-finder/app.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const db = require('./db/connection');
+
 const app = express();
 
 const PORT = 3000;
 
 app.listen(PORT, () => console.log(`O Express está rodando na porta ${PORT}`));
 
+/**
+ * db connection
+ */
+db.authenticate()
+  .then(() => console.log('Conectou ao banco com sucesso'))
+  .catch(() => console.log('Ocorreu um erro ao conectar'));
+
+/**
+ * Routes
+ */
 app.get('/', (req, res) => res.send('Está funcionando 3'));
